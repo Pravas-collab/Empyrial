5fc9db9247a45133103b7b29dc6dc19340bb01b1
Pravas-collab <80254804+Pravas-collab@users.noreply.github.com>
2022-06-21 16:58:08 +0800

    Update empyrial_test.py

--- Empyrial/test/empyrial_test.py
+++ Empyrial/test/empyrial_test.py
@@ -1,7 +1,7 @@
 from empyrial import empyrial, Engine
 
 portfolio = Engine(    
-                  start_date= "2018-06-09", 
+                  start_date= "2020-06-09", 
                   portfolio= ["BABA", "RELIANCE.NS", "KO", "^DJI","^IXIC"], 
                   weights = [0.2, 0.2, 0.2, 0.2, 0.2], #equal weighting by default
                   benchmark = ["SPY"] #SPY by default
